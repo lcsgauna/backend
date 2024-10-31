deeb01e8dc6e8a0e6544043ccffc6ef9f5672f0b
Lucas Gauna Samudio <lucasgsamudio@gmail.com>
2024-10-31 13:29:40 -0400

    feat: Ajustando CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3040);
 }
 bootstrap();
